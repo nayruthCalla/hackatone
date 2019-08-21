2e2521b7c52d1ecf5c13b286984198375515f89c
nayruthCalla <nayruth.calla@gmail.com>
2019-08-21 15:29:06 -0500

    Updates

---
+++ precache-manifest.f092199588e5871003ee15b7f61e962b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0457f9973358af032693a0bc5680fb",
+    "url": "/nayruthCalla/hackatone/index.html"
+  },
+  {
+    "revision": "82d8468436c816673cd4",
+    "url": "/nayruthCalla/hackatone/static/css/main.48bd904f.chunk.css"
+  },
+  {
+    "revision": "557f6986d7a6e745e30c",
+    "url": "/nayruthCalla/hackatone/static/js/2.2823ffe9.chunk.js"
+  },
+  {
+    "revision": "82d8468436c816673cd4",
+    "url": "/nayruthCalla/hackatone/static/js/main.492a1448.chunk.js"
+  },
+  {
+    "revision": "448e8ad63718926f5c6d",
+    "url": "/nayruthCalla/hackatone/static/js/runtime~main.70b2a5f1.js"
+  },
+  {
+    "revision": "505ee41bbdfedb6ff2494176502fdbb9",
+    "url": "/nayruthCalla/hackatone/static/media/bq.505ee41b.jpg"
+  },
+  {
+    "revision": "b148b6b74082f53db2f9f6d83586f7bc",
+    "url": "/nayruthCalla/hackatone/static/media/catalogo.b148b6b7.pdf"
+  },
+  {
+    "revision": "953e3ce9b9f922ed5daff9c5e9f26eff",
+    "url": "/nayruthCalla/hackatone/static/media/chef.953e3ce9.png"
+  },
+  {
+    "revision": "b072de2acfd69a0689840b4354cde014",
+    "url": "/nayruthCalla/hackatone/static/media/hamburger.b072de2a.png"
+  },
+  {
+    "revision": "c07b0a7510a68ebea1b6f978fc8e9062",
+    "url": "/nayruthCalla/hackatone/static/media/waitress.c07b0a75.png"
+  }
+]);

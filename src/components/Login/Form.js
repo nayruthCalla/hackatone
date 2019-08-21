86b74b06826c5f755a3bcfe96cc044c81a813755
nayruthCalla <nayruth.calla@gmail.com>
2019-08-21 06:17:13 -0500

    login-icon

--- src/components/Login/Form.js
+++ src/components/Login/Form.js
@@ -35,8 +35,7 @@ const Form = ({ logprop }) => {
         value={email}
         label='Email'
         update={(e) => setEmail(e.target.value)}
-        placeholder='Email'
-        icon='fas fa-user'
+        placeholder='Email'        
         classValue='emailValue form-control border-none radius-50'
         visibility="input-group-append border-none radius-50" />
       <Inputs
@@ -45,8 +44,7 @@ const Form = ({ logprop }) => {
         value={password}
         label='Password'
         update={(e) => setPassword(e.target.value)}
-        placeholder='Password'
-        icon='fas fa-lock'
+        placeholder='Password'        
         classValue='passwordValue form-control border-none radius-50'
         visibility="input-group-append border-none radius-50"
       />
